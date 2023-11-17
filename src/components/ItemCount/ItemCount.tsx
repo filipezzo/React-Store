49217688220fb105c25190b46d0e3233201aea83
Filipe Avanzzo <avanzzodev@gmail.com>
2023-11-17 00:08:37 -0300

    componente de contador de itens feito

---
+++ src/components/ItemCount/ItemCount.tsx
@@ -0,0 +1,65 @@
+import { Minus, Plus } from "phosphor-react";
+import { useEffect, useState } from "react";
+
+interface Props {
+  stock: number;
+  initial: number;
+}
+
+function ItemCount({ stock, initial }: Props) {
+  const [items, setItems] = useState(1);
+
+  useEffect(() => {
+    if (initial < stock) {
+      setItems(initial);
+    }
+  }, [initial, stock]);
+
+  const handleMinusClick = () => {
+    items > 1 && setItems((prev) => prev - 1);
+  };
+
+  const handlePlusClick = () => {
+    items < stock && setItems((prev) => prev + 1);
+  };
+
+  const disableBtn = items === stock || stock === 0;
+  const btnClass = disableBtn
+    ? "bg-gray-500  text-white px-4 py-2 rounded-lg cursor-not-allowed opacity-60"
+    : "bg-blue-500  text-white px-4 py-2 rounded-lg ";
+
+  return (
+    <div>
+      {stock ? (
+        <>
+          <div className="flex items-center gap-2  ">
+            <button
+              onClick={handleMinusClick}
+              className="text-blue-500 text-2xl"
+            >
+              <Minus />
+            </button>
+            <span>{items}</span>
+            <button
+              onClick={handlePlusClick}
+              className="text-blue-500 text-2xl"
+            >
+              <Plus />
+            </button>
+          </div>
+          <button disabled={disableBtn} className={btnClass}>
+            Add {items} to cart
+          </button>
+        </>
+      ) : (
+        <button disabled={disableBtn} className={btnClass}>
+          Apologies, currently out of stock! 😿
+        </button>
+      )}
+
+      {}
+    </div>
+  );
+}
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.tsx
+++ src/components/ItemListContainer/ItemListContainer.tsx
@@ -1,3 +1,5 @@
+import ItemCount from "../ItemCount/ItemCount";
+
 interface Props {
   greeting: string;
 }
@@ -8,6 +10,10 @@ function ItemListContainer({ greeting }: Props) {
       <p className="mt-10 text-2xl text-blue-500 font-bold text-center">
         {greeting}
       </p>
+      <ItemCount stock={5} initial={12} />
+      <ItemCount stock={10} initial={4} />
+      <ItemCount stock={6} initial={2} />
+      <ItemCount stock={0} initial={0} />
     </section>
   );
 }
